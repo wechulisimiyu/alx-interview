cbb00795b0252ce2b3175acf7bb0de6d84f65969
wechu07 <wechu017@gmail.com>
2022-05-02 21:02:54 +0300

    logic correction

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,36 +1,59 @@
 #!/usr/bin/python3
-''' module for log parsing '''
-
-
+"""
+Script that reads stdin line by line and computes metrics:
+- Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status
+                code> <file size>
+- After every 10 lines and/or a keyboard interruption (CTRL + C), print these
+    statistics from the beginning:
+Example:
+    File size: 5213
+    200: 2
+    401: 1
+    403: 2
+    404: 1
+    405: 1
+    500: 3
+"""
 import sys
-total = 0
-counter = 0
-sc_dict = {'200': 0, '301': 0, '400': 0, '401': 0,
-           '403': 0, '404': 0, '405': 0, '500': 0}
+stcd = {"200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0}
+summ = 0
 
 
-def print_data(total):
-    ''' function to print statistics for input '''
-    print('File size: {}'.format(total))
-    for key, value in sorted(sc_dict.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+def prn_stats():
+    """
+    Function that print stats about log
+    """
+    global summ
+
+    print('File size: {}'.format(summ))
+    stcdor = sorted(stcd.keys())
+    for each in stcdor:
+        if stcd[each] > 0:
+            print('{}: {}'.format(each, stcd[each]))
 
 
-try:
-    for line in sys.stdin:
-        rline = line.split(" ")
-        if len(rline) > 4:
-            code = rline[-2]
-            if code in sc_dict.keys():
-                sc_dict[code] += 1
-            filesize = int(rline[-1])
-            total += filesize
-            counter += 1
-        if counter == 10:
-            counter = 0
-            print_data(total)
-except Exception as ex:
-    pass
-finally:
-    print_data(total)
+if __name__ == "__main__":
+    cnt = 0
+    try:
+        """ Iter the standar input """
+        for data in sys.stdin:
+            try:
+                fact = data.split(' ')
+                """ If there is a status code """
+                if fact[-2] in stcd:
+                    stcd[fact[-2]] += 1
+                """ If there is a lenght """
+                summ += int(fact[-1])
+            except:
+                pass
+            """ Printing control """
+            cnt += 1
+            if cnt == 10:
+                prn_stats()
+                cnt = 0
+    except KeyboardInterrupt:
+        prn_stats()
+        raise
+    else:
+        prn_stats()
