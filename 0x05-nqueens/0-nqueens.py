a45fb9050d0aa0d7ff709a696444521ce48f65a0
wechu07 <wechu017@gmail.com>
2022-05-13 20:57:57 +0300

    long time no see

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,57 +1,62 @@
 #!/usr/bin/python3
-
 import sys
 
 
-def solve(row, column):
-    solver = [[]]
-    for q in range(row):
-        solver = place_queen(q, column, solver)
-    return solver
-
-
-def place_queen(q, column, prev_solver):
-    solver_queen = []
-    for array in prev_solver:
-        for x in range(column):
-            if is_safe(q, x, array):
-                solver_queen.append(array + [x])
-    return solver_queen
-
-
-def is_safe(q, x, array):
-    if x in array:
-        return (False)
-    else:
-        return all(abs(array[column] - x) != q - column
-                   for column in range(q))
-
-
-def init():
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    if sys.argv[1].isdigit():
-        the_queen = int(sys.argv[1])
-    else:
-        print("N must be a number")
-        sys.exit(1)
-    if the_queen < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    return(the_queen)
-
-
-def n_queens():
-
-    the_queen = init()
-    solver = solve(the_queen, the_queen)
-    for array in solver:
-        clean = []
-        for q, x in enumerate(array):
-            clean.append([q, x])
-        print(clean)
-
-
 if __name__ == '__main__':
-    n_queens()
+    if len(sys.argv) > 1:
+        try:
+            # number of queens we are solving for
+            board_size = int(sys.argv[1])
+        except Exception:
+            print("N must be a number")
+            exit(1)
+        if board_size < 4:
+            print("N must be at least 4")
+            exit(1)
+
+        # will hold current testing data
+        currentSolution = [0 for x in range(board_size)]
+
+        # found solutions
+        solutions = []
+
+        def isSafe(testRow, testCol):
+            # no need to check for row 0
+            if testRow == 0:
+                return True
+
+            for row in range(0, testRow):
+
+                # check vertical
+                if testCol == currentSolution[row]:
+                    return False
+
+                # diagonal
+                if abs(testRow - row) == abs(testCol - currentSolution[row]):
+                    return False
+
+            # no attack found
+            return True
+
+        def placeQueen(row):
+            global currentSolution, solutions, board_size
+            for col in range(board_size):
+                if not isSafe(row, col):
+                    continue
+                else:
+                    currentSolution[row] = col
+                    if row == (board_size - 1):
+                        #  last row
+                        solution = []
+                        for i in range(len(currentSolution)):
+                            solution.append([i, currentSolution[i]])
+                        solutions.append(solution)
+                    else:
+                        placeQueen(row + 1)
+
+        placeQueen(0)
+        for solution in solutions:
+            print(solution)
+    else:
+        print("Usage: nqueens N")
+        exit(1)
